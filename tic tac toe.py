a478e85842d5c70a94f99b1069278e5a9f312763
amir fenina <146543414+amirfenina@users.noreply.github.com>
2023-10-19 10:48:29 +0100

    tic tac toe.py
    problem fixed

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,3 +1,5 @@
+import sys
+
 class TicTacToe:
     def __init__(self):
         self.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -20,86 +22,37 @@ class TicTacToe:
                 print('Please enter a number 1-9 that is not taken')
 
     def check_win(self):
-        if board[0] == board[1] == board[2]:
-            if board[0] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
+        for i in range(3):
+            if self.board[i] == self.board[i + 3] == self.board[i + 6] or \
+               self.board[i * 3] == self.board[i * 3 + 1] == self.board[i * 3 + 2]:
+                winner = self.board[i]  # Get the winner
+                print(winner, 'WIN')
+                self.end_game()
+                return True
+
+        if self.board[0] == self.board[4] == self.board[8] or \
+           self.board[2] == self.board[4] == self.board[6]:
+            winner = self.board[4]  # Get the winner
+            print(winner, 'WIN')
+            self.end_game()
             return True
-        if board[3] == board[4] == board[5]:
-            if board[3] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
+
+        if all(isinstance(cell, str) for cell in self.board):
+            print('IT\'S A DRAW')
+            self.end_game()
             return True
-        if board[6] == board[7] == board[8]:
-            if board[6] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
-            return True
-        if board[0] == board[3] == board[6]:
-            if board[0] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
-            return True
-        if board[1] == board[4] == board[7]:
-            if board[1] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
-            return True
-        if board[2] == board[5] == board[8]:
-            if board[2] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
-            return True
-        if board[0] == board[4] == board[8]:
-            if board[0] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
-            return True
-        if board[2] == board[4] == board[6]:
-            if board[2] == 'X':
-                print('X WIN')
-                endgame()
-            else:
-                print('O WIN')
-                endgame()
-            return True
-        if board.count('X')+board.count('O') == 9:
-            print('IS A DRAW')
-            endgame()
-            return True
-        else:
-            return False
-        pass
+
+        return False
 
     def end_game(self):
         again = input('\nDo you want to play again? (Y/n)')
         if again == 'Y':
             self.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
             self.current_player = 'X'
-            self.start_game()
+            self.display_board()
+            self.pick_position()
         elif again == 'n':
-            return
+            sys.exit(0)
         else:
             print("Please answer Y or n")
             self.end_game()
@@ -108,7 +61,6 @@ class TicTacToe:
         print("Welcome to the XO game! (Tic Tac Toe)")
         self.display_board()
         self.pick_position()
-
 
 if __name__ == "__main__":
     game = TicTacToe()
